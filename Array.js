854d15467e575d21d8c407322e56dc3075395885
IgorBosin <ibosin@yandex.ru>
2023-01-23 10:49:37 +0300

    addExample#30

--- Array.js
+++ Array.js
@@ -752,4 +752,14 @@ function findAverage(array) {
 }
 
 console.log(findAverage([1, 2, 3, 4])) // 2.5
-    
+// ------------------------------------
+
+
+
+// Задача №30 Проверить содержит ли массив определенное значение (8 kyu No Loops 2 - You only need one)   
+
+function check(a, x) {
+    return a.includes(x)
+};
+console.log(check([80, 117, 115, 104, 45, 85, 112, 115], 45)) // true
+console.log(check(['what', 'a', 'great', 'kata'], 'kat')) // false
